26e7a2a8eaf4bcfa222b2d5f2306a91389a49b9f
EvilisGlenio <glenio.developer@gmail.com>
2025-08-05 21:55:13 -0300

    🌈 style: Dark Theme Default configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import "./App.css";
+import { Button } from "./components/ui/button";
 
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
 }
